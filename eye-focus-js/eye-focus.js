ca4ecaaa8c96a57166fcecc67394f735ddc3e3c1
Dustin McQuay <dmcquay@gmail.com>
2011-01-28 11:54:11 -0700

    Fix bug in example, add brief docs.

--- eye-focus-js/eye-focus.js
+++ eye-focus-js/eye-focus.js
@@ -1,3 +1,21 @@
+/**
+ * Provides the EyeFocus object. This object tracks the visibility of a provided set of
+ * elements. When an element becomes visible, the "visible" event is triggered on that
+ * element. Here are the events triggered:
+ *
+ *		visible:				when the element is visible
+ *		not-visible:		when the element is not visible
+ *		top-visible:		the visible element that is highest on the page
+ *		bottom-visible:	the visible element that is lowest on the page
+ *
+ * All events are triggered at the time of the document's scroll event. Currently, events
+ * are called every time, not just deltas (e.g. if an element is already visible, the visible
+ * event will be called on it again on the next scroll).
+ *
+ * Author: Dustin McQuay
+ * License: MIT
+ */
+
 EyeFocus = function(elements) {
 	var self = this;
 	if(! elements instanceof Array) {
@@ -37,11 +55,11 @@ EyeFocus.prototype.detectVisibleElements = function() {
 
 EyeFocus.prototype.isElementVisible = function(elem) {
 	var elemTop = this.jQuery(elem).offset().top;
-    var elemBottom = elemTop + this.jQuery(elem).height();
-    
-    var docViewTop = this.jQuery(window).scrollTop();
-    var docViewBottom = docViewTop + this.jQuery(window).height();
-
-    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
-      && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
+  var elemBottom = elemTop + this.jQuery(elem).height();
+  
+  var docViewTop = this.jQuery(window).scrollTop();
+  var docViewBottom = docViewTop + this.jQuery(window).height();
+  
+  return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
+    && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
 };
